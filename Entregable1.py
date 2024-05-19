ee6705017e0a6f698abc7853111b3cbde3056df6
Yuls <yuleidissalgado042@gmail.com>
2024-05-19 18:05:20 -0500

    Definición de primer esquema

---
+++ Entregable1.py
@@ -0,0 +1,33 @@
+import random 
+def escoger_palabras():
+    palabras = ['python', 'programacion', 'ahorcado', 'entregable', 'funciones']
+    return palabras[random.randint(0, len(palabras) - 1)]
+
+escoger_palabras() #???
+
+# Con Len se le dice a random.randint que debe ir desde el indice cero hasta el final de la longitud de la palabra 
+
+def juego_esquema():
+    palabra = escoger_palabras()
+    intentos = 6 #No sirve porque es una funcion, pero antes si servia
+    letras_intentadas = []
+    palabra_adivinada = ["_"] * len(palabra) #Var local, no global. Fix it >> Quitar la tercera funcion
+
+    print("¡Bienvenido al juego del ahorcado!")
+
+    while intentos > 0: #Necesita un bucle, while?
+        print(f"La palabra a adivinar es: {palabra_adivinada}")#No muestra los guiones
+        print (f"Letras intentadas: {letras_intentadas}")
+        print (f"Te quedan {intentos} intentos") 
+        break       
+        
+    letra_escogida == 1 and "a" <= letra_escogida <= "z"
+    letra_escogida = input(f"Ingrese la letra escogida: ").lower
+        
+    if letra_escogida == "palabra":
+        print(f"Felicidades. Aún te quedan ?palabras por adivinar")
+    else:
+        print("Intenta con una nueva letra")
+
+juego_esquema()
+
