51dc32d11cfe846e870402e1c892235ea5c53f67
Yuls <yuleidissalgado042@gmail.com>
2024-05-24 22:03:30 -0500

    Docstrings

--- Entregable1.py
+++ Entregable1.py
@@ -13,10 +13,18 @@ def dibujo_juego():
     return dibujo
 
 def escoger_palabras():
+    """
+    Función que devuelve una palabra aleatoria de una lista de palabras
+    
+    Parámetro: 
+    None 
+    
+    Returns: 
+    str: Una palabra random desde una lista predefinida de palabras.
+    """
     palabras = ['python', 'programacion', 'ahorcado', 'entregable', 'funciones']
     #palabra_facil= {'casa', 'letra', 'rojo', 'azul', 'lapiz'}
     return palabras[random.randint(0, len(palabras)  - 1)]
-# Con Len se le dice a random.randint que debe ir desde el indice cero hasta el final de la longitud de la palabra 
 
 def juego_esquema():
     palabra = escoger_palabras()
@@ -29,27 +37,30 @@ def juego_esquema():
     print ("=============================================================================\n")
     
     print("Simulación del clásico juego del 'Ahorcado', donde se dispone de varios intentos para adivinar la palabra aleatoria.\n")
-    print(f"Siguiendo este esquema:\n{dibujo_juego()}\n")
-    print("*Instrucciones*:writing_hand:: Los intentos son proporcionales a la longitud de la palabra aleatoria.")
+    #print(f"Siguiendo este esquema:\n{dibujo_juego()}\n")
+    print("Instrucciones✍: Los intentos son proporcionales a la longitud de la palabra aleatoria.")
     print("Al obtener un acierto, se suma un punto; caso contrario, al perder, se van restando vidas.\n")
 
-    datos_entrada = (str(input("¿Te gustaría jugar (Si / No): ")).strip()).capitalize() #Eliminar espacios, anticipar fallas
-
-    if datos_entrada.capitalize() == "Si":
-        print("Muchas gracias por su respuesta")
-        nombre_usuario = str(input("Por favor ingrese su nombre: ")).capitalize()
-        print(f"¡Bienvenidx al juego, {nombre_usuario}!")
-        
-    else:
-        print("Ni modo")
+    datos_entrada = (str(input("¿Te gustaría jugar (Si / No): ")).capitalize()).strip() #Eliminar espacios, anticipar fallas
     
-    #Arreglar el try and except. Tal vez usar while para repetir el mensaje y que no cierre el zip
-        print("Solo puedes elegir Sí o no :face_with_rolling_eyes:")
+    while True:
+        try:
+            if datos_entrada.capitalize() == "Si":
+                print("Muchas gracias por su respuesta")
+                nombre_usuario = str(input("Por favor ingrese su nombre: ")).capitalize()
+                print(f"¡Bienvenidx al juego, {nombre_usuario}!")
+                StopIteration
+            else:
+                print("Gracias por participar. ¡Esperamos te animes a una próxima!")
+
+        except datos_entrada not in "Si / No":
+            print("Solo puedes elegir Sí o no 🙄")
      
-    #while intentos >= 0: #Necesita un bucle, while?
-        #print(f"La palabra a adivinar es: {palabra_adivinada}")#No muestra los guiones
-        #print (f"Letras intentadas: {letras_intentadas}")
-        #print (f"Te quedan {intentos} intentos")    
+    while intentos >= 0 and datos_entrada == "Si":
+        # ??? No tiene sentido
+        print(f"La palabra a adivinar es: {palabra_adivinada}") 
+        print (f"Letras intentadas: {letras_intentadas}")
+        print (f"Te quedan {intentos} intentos")    
         
     if datos_entrada == "Si":
         letra_escogida = str(input(f"Ingrese la letra escogida: ")).lower()
--- README.txt
+++ README.txt
@@ -1,2 +1,21 @@
-Hola :) 
+Hola :) ¿Qué tal?
 
+¡Bienvenido al juego de adivina la palabra! 👩‍💻
+   
+Simulación del clásico juego del 'Ahorcado', donde se dispone de varios intentos para adivinar la palabra aleatoria.
+
+Instrucciones✍: Los intentos son proporcionales a la longitud (menos 1 vida) de la palabra aleatoria.
+Al obtener un acierto, se suma un punto; caso contrario, al perder, se van restando vidas.
+
+Para abordar el juego se sigue el siguiente esquema:
+Juego = [""""
+               ------
+               |    |
+               |    O
+               |   /|\\
+               |   / \\
+               |
+            ------
+            """]
+
+¡Espero que te diviertas! 🖐
