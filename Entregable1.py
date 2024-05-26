20dd0979c8e818631e33e3170f206ad47e2be7fc
Yuls <yuleidissalgado042@gmail.com>
2024-05-26 15:27:35 -0500

    Arreglando secuencia while true

--- Entregable1.py
+++ Entregable1.py
@@ -30,6 +30,7 @@ def juego_esquema():
     palabra = escoger_palabras()
     intentos = len(palabra) - 1
     letras_intentadas = []
+    aciertos = []
     palabra_adivinada = ["_"] * len(palabra) 
 
     print ("=============================================================================")
@@ -40,41 +41,45 @@ def juego_esquema():
     #print(f"Siguiendo este esquema:\n{dibujo_juego()}\n")
     print("Instrucciones✍: Los intentos son proporcionales a la longitud de la palabra aleatoria.")
     print("Al obtener un acierto, se suma un punto; caso contrario, al perder, se van restando vidas.\n")
-
-    datos_entrada = (str(input("¿Te gustaría jugar (Si / No): ")).capitalize()).strip() #Eliminar espacios, anticipar fallas
     
     while True:
         try:
+            datos_entrada = (str(input("¿Te gustaría jugar (Si / No): ")).capitalize()).strip() #Eliminar espacios, anticipar fallas
+            if datos_entrada.capitalize() not in ["Si", "No"]:
+                raise ValueError ("Solo puedes elegir Sí o no 🙄")
+            
             if datos_entrada.capitalize() == "Si":
                 print("Muchas gracias por su respuesta")
                 nombre_usuario = str(input("Por favor ingrese su nombre: ")).capitalize()
                 print(f"¡Bienvenidx al juego, {nombre_usuario}!")
-                StopIteration
+                print(f"La palabra a adivinar es: {palabra_adivinada}") 
+                break
+
             else:
                 print("Gracias por participar. ¡Esperamos te animes a una próxima!")
+                return
 
-        except datos_entrada not in "Si / No":
-            print("Solo puedes elegir Sí o no 🙄")
+        except ValueError as e:
+            print(e)
+            
      
-    while intentos >= 0 and datos_entrada == "Si":
+    #while intentos >= 0 and datos_entrada == "Si":
         # ??? No tiene sentido
-        print(f"La palabra a adivinar es: {palabra_adivinada}") 
-        print (f"Letras intentadas: {letras_intentadas}")
-        print (f"Te quedan {intentos} intentos")    
-        
     if datos_entrada == "Si":
         letra_escogida = str(input(f"Ingrese la letra escogida: ")).lower()
+
+    while intentos >= 0 and aciertos == len(palabra):
+        print (f"Letras intentadas: {letras_intentadas}")
+        print (f"Te quedan {intentos} intentos")    
 
         if len(letra_escogida) == 1 and 'a' <= letra_escogida <= 'z':
             if letra_escogida in letras_intentadas:
                 print("Ya intentaste esa letra.")
             elif letra_escogida in palabra:
                 print(f"Felicidades, adivinaste una letra. Aún te quedan {intentos} por adivinar")
+                aciertos =+ 1
         else: 
             letras_intentadas.append(letra_escogida)
             print("Intenta con una nueva letra")
 
 juego_esquema() 
-
-print("\033[4;;47m" + "Esto es una prueba de color" + "\033[0;m") #Video de youtube
-#La extensión de emojis no funcionó, borrar
